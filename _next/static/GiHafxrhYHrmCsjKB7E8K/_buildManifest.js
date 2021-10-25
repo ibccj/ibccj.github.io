e99b9c3521572ced485484cc38caa43583775d77
mfuentesg <marceloe.fuentes@gmail.com>
2021-10-25 22:08:45 +0000

    Update gh-pages to output generated at 7fb0b6a

---
+++ _next/static/GiHafxrhYHrmCsjKB7E8K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/859-86e5279a2b26a4a2bea6.js","static/chunks/pages/index-8318b609332001e61524.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
